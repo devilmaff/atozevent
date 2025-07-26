a284db53d45a7a3ef4c3e9a2038d2bd493ec7bca
devilmaff <devilblackkinghusain.com>
2025-07-26 10:20:38 +0530

    site

--- server.js
+++ server.js
@@ -138,4 +138,4 @@ app.post('/delete/:id', async (req, res) => {
 });
 
 // ✅ Start the server
-app.listen(3000, () => console.log("✅ Server running at http://localhost:3000"));
+app.listen(3000, () => console.log("✅ Server running at https://atozevents.netlify.app/"));
